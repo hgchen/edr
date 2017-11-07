b1478e2d7efec68b5503a5420f54b55ca8149d03
George Chen <huaijin.chen@gmail.com>
2017-11-07 16:02:56 -0600

    Minor parameter updates

--- edr_net.py
+++ edr_net.py
@@ -4,6 +4,28 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 
+
+class EMA(nn.Module):
+
+    def __init__(self, alpha=0.3, learnable=False):
+        super(EMA, self).__init__()
+        alpha = torch.from_numpy(np.asarray([alpha], dtype=float))
+        alpha = alpha.type(torch.FloatTensor)
+        self.alpha = Variable(alpha, requires_grad=learnable)
+
+
+    def forward(self, input, hidden):
+
+
+
+        hidden = self.alpha.expand_as(input) * input + (1 - self.alpha).expand_as(input) * hidden
+
+        return hidden
+
+    # def initHidden(self):
+    #     return Variable(torch.zeros(1, ))
+
+
 class EMARecurrent(nn.Module):
 
     def __init__(self, alpha=0.4, learnable=False):
@@ -27,31 +49,16 @@ class EMARecurrent(nn.Module):
         return output
 
 
-class EMA(nn.Module):
-
-    def __init__(self, alpha=0.3, learnable=False):
-        super(EMA, self).__init__()
-        alpha = torch.from_numpy(np.asarray([alpha], dtype=float))
-        alpha = alpha.type(torch.FloatTensor)
-        self.alpha = Variable(alpha, requires_grad=learnable)
-
-
-    def forward(self, input, hidden):
-
-
-
-        hidden = torch.abs(self.alpha).expand_as(input) * input + (1 - torch.abs(self.alpha).expand_as(input)) * hidden
-
-        return hidden
-
-    # def initHidden(self):
-    #     return Variable(torch.zeros(1, ))
-
-
 class EDR(nn.Module):
-    def __init__(self, alpha=0.3, learnable=False):
+    """
+    alpha = 0.5 and 0.0166
+    
+    """
+    def __init__(self, alpha=0.5, learnable=False, mu_on=0.05, mu_off=-0.1):
         super(EDR, self).__init__()
         self.ema_recurrent = EMARecurrent(alpha, learnable)
+        self.mu_on = mu_on
+        self.mu_off = mu_off
 
 
     def forward(self, input, hidden):
@@ -61,9 +68,10 @@ class EDR(nn.Module):
 
         x = x / ema
         x = torch.log(x)
-        x = x * 20
-        pos = F.relu(x - 0.01).unsqueeze(2)
-        neg = F.relu(-x - 0.01).unsqueeze(2)
+        x = F.tanh(x)
+        # x = x * 20
+        pos = F.relu(x - self.mu_on).unsqueeze(2)
+        neg = F.relu(-(x - self.mu_off)).unsqueeze(2)
 
         x = torch.cat((pos, neg), dim=2)
 
--- edr_train.py
+++ edr_train.py
@@ -7,7 +7,7 @@ input = Variable(torch.rand(1, 20,9,9))
 hidden = Variable(torch.rand(1, 1,9,9))
 output = []
 
-model = edr_net.EDR(alpha=0.4, learnable=False)
+model = edr_net.EDR(alpha=0.4, learnable=False, mu_on=0.05, mu_off=-0.1)
 output = model(input,hidden)
 
 print(output)
